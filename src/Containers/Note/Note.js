8c0077eeb37dce1c5992763724c719ae237e1056
Kelly Zick <kllyzck@gmail.com>
2019-05-23 21:38:38 -0600

    WIP Note Card component

--- src/Containers/Note/Note.js
+++ src/Containers/Note/Note.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { mockNotes } from '../../utils/mockData';
 
 export class Note extends Component {
-  
 
   render() {
+    const { title, listItems } = mockNotes[0]
+    const { text, id } = listItems
+    const list = listItems.map(li => {
+      return (
+        <li className='list-item'>
+          <input 
+            type="checkbox" 
+            className="checkbox" 
+            id={`item-${id}`} 
+            {...completed && 'checked'}
+            value={listItems.text}
+            onChange={() => this.handleCheckbox(id)}
+            />
+          <label 
+            for={`item-${id}`} 
+            contentEditable
+            onChange={() => this.handleTextChange(id)}>
+            {text}
+        </label>
+      </li>
+      )
+    })
     return (
-      <article>
+      <article 
+        className='note-card'>
+        <h3>{title}</h3>
+        
         
       </article>
     )
